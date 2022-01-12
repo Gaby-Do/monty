a411e584efb0218ee6d83cc4036b2a6c356034ce
Gaby-Do <eme.gaby.do@gmail.com>
2022-01-12 06:56:30 -0800

    agrega funciones en opcode_imp

--- opcode_implementation.c
+++ opcode_implementation.c
@@ -23,8 +23,15 @@ void _push(stack_t **stack, unsigned int line_number)
 
 void _pall(stack_t **stack, unsigned int line_number)
 {
-	(void) (*stack);
 	(void) line_number;
+
+	if (!*stack)
+		return;
+	while (*stack)
+	{
+		printf("%d\n", (*stack)->n);
+		*stack = (*stack)->next;
+	}
 	printf("this the _pall function\n");
 }
 
@@ -37,8 +44,12 @@ void _pall(stack_t **stack, unsigned int line_number)
 
 void _pint(stack_t **stack, unsigned int line_number)
 {
-	(void) (*stack);
-	(void) line_number;
+	if (!*stack)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
 	printf("this the _pint function\n");
 }
 
@@ -51,8 +62,18 @@ void _pint(stack_t **stack, unsigned int line_number)
 
 void _pop(stack_t **stack, unsigned int line_number)
 {
-	(void) (*stack);
-	(void) line_number;
+	stack_t *aux;
+
+	if (!stack || !*stack)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	aux = *stack;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(aux);
+
 	printf("this the _pop function\n");
 }
 
@@ -65,7 +86,17 @@ void _pop(stack_t **stack, unsigned int line_number)
 
 void _swap(stack_t **stack, unsigned int line_number)
 {
-	(void) (*stack);
-	(void) line_number;
+	int x = 0;
+	int y = 0;
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	x = (*stack)->n;
+	y = (*stack)->next->n;
+	(*stack)->n = y;
+	(*stack)->next->n = x;
 	printf("this the _swap function\n");
 }
