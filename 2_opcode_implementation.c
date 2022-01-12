217361f402c7eac11346fe0ca040472b1a69e8bd
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-01-12 06:59:01 -0800

    add sub

--- 2_opcode_implementation.c
+++ 2_opcode_implementation.c
@@ -24,7 +24,6 @@ void _add(stack_t **stack, unsigned int line_number)
 	sum = num1 + num2;
 	_pop(stack, line_number);
 	(*stack)->n = sum; /**ver si era este o si era solo -> n*/
-
 }
 
 /**
@@ -38,4 +37,32 @@ void _nop(stack_t **stack, unsigned int line_number)
 {
 	(void) (*stack);
 	(void) line_number;
+}
+
+/**
+ *_sub - sub the top element of the stack from the second top element.
+ *@stack: doble pointer to an array
+ *@line_number: line read
+ *Return: nothing
+ */
+
+void _sub(stack_t **stack, unsigned int line_number)
+{
+	int num1 = 0;
+	int num2 = 0;
+	int sub = 0;
+
+	printf("this the _sub function\n");
+	if (!*stack || !(*stack)->next)
+		{
+			fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+	num1 = (*stack)->n;
+	num2 = (*stack)->next->n;
+	sub = num1 - num2;
+	if (sub < 0)
+		sub = sub * -1;
+	_pop(stack, line_number);
+	(*stack)->n = sub; /**ver si era este o si era solo -> n*/
 }
