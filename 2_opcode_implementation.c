87255c6e0bd76b612828d53403e1207495bc8c56
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-01-12 06:45:33 -0800

    funcion add

--- 2_opcode_implementation.c
+++ 2_opcode_implementation.c
@@ -9,9 +9,22 @@
 
 void _add(stack_t **stack, unsigned int line_number)
 {
-	(void) (*stack);
-	(void) line_number;
+	int num1 = 0;
+	int num2 = 0;
+	int sum = 0;
+
 	printf("this the _add function\n");
+	if (!*stack || !(*stack)->next)
+		{
+			fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+	num1 = (*stack)->n;
+	num2 = (*stack)->next->n;
+	sum = num1 + num2;
+	_pop(stack, line_number);
+	(*stack)->n = sum; /**ver si era este o si era solo -> n*/
+
 }
 
 /**
@@ -25,5 +38,4 @@ void _nop(stack_t **stack, unsigned int line_number)
 {
 	(void) (*stack);
 	(void) line_number;
-	printf("this the _nop function\n");
 }
